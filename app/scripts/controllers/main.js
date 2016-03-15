4b359527ec5b5658a4c10b7bfd4b84f3d25648f8
Bluestella <hello@Josephbuluran.com>
2016-03-16 01:17:47 +0800

    new init.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sephcvApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sephcvApp
+ */
+angular.module('sephcvApp')
+  .controller('MainCtrl', function () {
+
+$scope.header = [{
+  "name": "Joseph Rannie M. Buluran",
+  "mobile":"",
+  "email":"jobs (at) josephbuluran (dot) com",
+  "website":"http://www.sepharoo.com/"
+}];
+
+$scope.summary = [{
+  "paragraph" : "With five years of thorough knowledge in Application Development with an extensive experience in web design and development, Electronic Data Interchange (EDI), Web Content Management Systems such as Adobe CQ5 and Enterprise Content Management (ECM) tools such as Alfresco, and Content Server."
+}];
+
+
+
+
+  });
