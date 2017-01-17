0ccde37f7da2fa74e85e15bbf11079880425706f
bluestella <joseph.buluran@gmail.com>
2017-01-17 14:30:46 +0800

    added mobile phone plus email real for redirect to mail.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,13 +12,14 @@ angular.module('sephcvApp')
 
 $scope.header = [{
   "name": "Joseph Rannie M. Buluran",
-  "mobile":"",
+  "mobile":"+639175010289",
   "email":"jobs (at) josephbuluran (dot) com",
+  "emailreal": "jobs@josephbuluran.com",
   "website":"http://www.sepharoo.com/"
 }];
 
 $scope.summary = [{
-  "paragraph" : "With six years of thorough knowledge in Application Development with an extensive experience in web design and development, Electronic Data Interchange (EDI), Web Content Management Systems such as Adobe CQ5 and Enterprise Content Management (ECM) tools such as Alfresco, and Content Server."
+  "paragraph" : "With six years of thorough knowledge in Application Development with an extensive experience in web design and development, Electronic Data Interchange (EDI), Web Content Management Systems such as Adobe CQ5 and Enterprise Content Management (ECM) tools such as Alfresco, and Content Server. Having an impressive track record of delivering major operational improvement and of orchestrating people, schedules and resources for optimum productivity, efficiency and quality. With people management skills and is responsible for the following: working with employees on career development, creating performance improvement plans, and holding monthly one on one meetings with each employee."
 }];
 
 
